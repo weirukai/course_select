20143b862cf0de179d6cf7410563eb4a80b3f917
weirukai <15623739350@163.com>
2020-10-14 20:15:58 +0800

    更改了导航栏颜色固定 更改了导航字体的大小 模拟大量课程

--- src/pages/layoutContainer/Aside.vue
+++ src/pages/layoutContainer/Aside.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="asideContainer main-backgroundColor">
     <el-menu
-        default-active="1"
+        :default-active="activeIndex"
         class="el-menu-vertical-demo main-backgroundColor"
         @open="handleOpen"
         @close="handleClose"
         @select="select"
         text-color="#fff"
-        active-text-color="#ffd04b"
+        active-text-color="#abcdef"
+        router
         >
       <!--router属性可以直接将menu-item的index映射成router的path-->
-      <template v-for="index in menu">
-        <el-menu-item :index="index.id" :key="index.id">
+      <template v-for="index in menu" >
+        <div v-bind:style="index.id!=activeIndex?'background-color:inherit':'background-color:#ecf5ff'" :key="index.id">
+          <el-menu-item :index="index.id" :key="index.id" class="menuItem">
+            <template slot="title">
+              <i :class="index.icon"></i>
+              <span slot="title">{{ index.name }}</span>
+              <div class="asideSelectedDivider" v-bind:style="activeIndex!=index.id?'background-color:inherit':'background-color:#abcdef'" ></div>
+            </template>
+          </el-menu-item>
 
-          <template slot="title">
-           <i :class="index.icon"></i>
-            <span slot="title">{{ index.name }}</span>
-          </template>
+        </div>
 
 
-        </el-menu-item>
+
+
+
+
+
       </template>
     </el-menu>
   </div>
@@ -32,7 +41,8 @@ export default {
   name: "Aside",
   data: function () {
     return {
-      menu: menu
+      menu: menu,
+      activeIndex:"generalCourse"
     }
 
   },
@@ -43,10 +53,11 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
-    //select(index, indexPath) {
-      // console.log(index, indexPath)
-      // console.log(index, indexPath)
-   // }
+    select(index) {
+      console.log(1)
+      this.activeIndex=index
+      console.log(2)
+   }
   }
 }
 
@@ -57,18 +68,19 @@ export default {
 .asideContainer {
   height: 100%;
 }
-.option-item{
-  font-size: 20px;
-  color: #333333;
-  line-height: 20px;
-  padding: 24px 0;
-  padding-top: 24px;
-  padding-right: 0px;
-  padding-bottom: 24px;
-  padding-left: 0px;
+.asideSelectedDivider{
+  height: 80%;
+  width: 2px;
+  background-color:inherit;
 }
-.menu-imageWrapper{
+.menuItem{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+
+}
 
 
-}
 </style>
